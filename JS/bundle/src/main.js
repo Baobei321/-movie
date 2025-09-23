18ede52e58537e67afe030e1f0dc1db36ff0e8e2
d1y <chenhonzhou@gmail.com>
2025-09-24 02:39:01 +0800

    fix: JS/bundle typeless

--- JS/bundle/src/main.js
+++ JS/bundle/src/main.js
@@ -7,7 +7,7 @@ const utils = {
    * @returns {string}
    */
   async getM3u8WithIframe(env) {
-    const iframe = env.get<string>("iframe")
+    const iframe = env.get("iframe")
     const html = await req(`${env.baseUrl}${iframe}`)
     const m3u8 = html.match(/"url"\s*:\s*"([^"]+\.m3u8)"/)[1]
     const realM3u8 = m3u8.replaceAll("\\/", "/")
